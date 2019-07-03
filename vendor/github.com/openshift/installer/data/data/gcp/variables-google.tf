bcbeafa29a22b148b529ffc7902014581a1c2a43
Corey Daley <cdaley@redhat.com>
2019-07-03 13:54:41 -0400

    removing openshift/installer dependency

--- vendor/github.com/openshift/installer/data/data/gcp/variables-google.tf
+++
@@ -1,9 +0,0 @@
-variable "google_project" {
-  type        = string
-  description = "The target GCP project for the cluster."
-}
-
-variable "google_region" {
-  type        = string
-  description = "The target GCP region for the cluster."
-}
