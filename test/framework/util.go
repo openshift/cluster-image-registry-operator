b3a8cc75933ee4f31ff0130af0f3826222d93cf8
Corey Daley <cdaley@redhat.com>
2019-08-19 09:54:19 -0400

    Don't set global proxy status manually
    We should not need to set the global proxy status manually  anymore

--- test/framework/util.go
+++ test/framework/util.go
@@ -3,11 +3,12 @@ package framework
 import (
 	"fmt"
 	"reflect"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 )
 
-func CheckEnvVars(want []corev1.EnvVar, have []corev1.EnvVar) []error {
+func CheckEnvVars(want []corev1.EnvVar, have []corev1.EnvVar, includes bool) []error {
 	var errs []error
 
 	for _, val := range want {
@@ -15,8 +16,14 @@ func CheckEnvVars(want []corev1.EnvVar, have []corev1.EnvVar) []error {
 		for _, v := range have {
 			if v.Name == val.Name {
 				found = true
-				if !reflect.DeepEqual(v, val) {
-					errs = append(errs, fmt.Errorf("environment variable contains incorrect data: expected %#v, got %#v", val, v))
+				if includes {
+					if !strings.Contains(v.Value, val.Value) {
+						errs = append(errs, fmt.Errorf("environment variable does not contain the expected value: expected %#v, got %#v", val, v))
+					}
+				} else {
+					if !reflect.DeepEqual(v, val) {
+						errs = append(errs, fmt.Errorf("environment variable does not equal the expected value: expected %#v, got %#v", val, v))
+					}
 				}
 			}
 		}
