bdecb6a3769e7891f3fbcf1269995af5b5449797
Adam Kaplan <adam.kaplan@redhat.com>
2020-01-07 14:37:46 -0500

    DEVEXP-433: Report image pruner's install status in Prometheus
    * Add a new Gauge metric which reports the installation status of the
    automatic impage pruner.
    * Alert if the image pruner is not installed and enabled
    * TEMP: report that the image pruner has been installed for CI to pass

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -5,15 +5,18 @@ import "github.com/prometheus/client_golang/prometheus"
 var (
 	registry            = prometheus.NewRegistry()
 	storageReconfigured = prometheus.NewCounter(prometheus.CounterOpts{
-		Namespace: "image_registry_operator",
-		Subsystem: "storage",
-		Name:      "reconfigured_total",
-		Help:      "Total times the image registry's storage was reconfigured.",
+		Name: "image_registry_operator_storage_reconfigured_total",
+		Help: "Total times the image registry's storage was reconfigured.",
+	})
+	imagePrunerInstallStatus = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "image_registry_operator_image_pruner_install_status",
+		Help: "Installation status code related to the automatic image pruning feature. 0 = not installed, 1 = suspended, 2 = enabled",
 	})
 )
 
 func init() {
 	registry.MustRegister(
 		storageReconfigured,
+		imagePrunerInstallStatus,
 	)
 }
