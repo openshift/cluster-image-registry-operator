234698e417ba148c316ebb27839c77f065fe07c0
Oleg Bulatov <oleg@bulatov.me>
2024-02-09 16:28:51 +0100

    pkg/operator: add AzurePathFix controller to migrate blobs
    this fixes the path bug on the storage level by migrating blobs from the
    `docker` virtual directory into the `/docker` virtual directory instead.
    
    * azurepathfix: give job required env vars
    * handle different job conditions on azure path fix controller
    * azurepathfixjob: set restart policy to never
    * azurepathfixjob: export account key from secret
    * azurepathfixjob: add azure account key env var to job container
    * pass account key to path fix job (this time for reals)
    * azurepathfix: only allow new controller to run on Azure
    * also mount trusted-ca volume to job so the script works when a
      cluster-wide proxy is in use.
    * fix a couple of mispellings
    * manifests: export operator image env var to container
    * manifests: update generated files
    * mount volumes for proxy certs on azure path fix job
    * azurepathfixcontroller: return error if account name is not present

---
+++ pkg/resource/azurepathfixjob.go
@@ -0,0 +1,241 @@
+package resource
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	kcorev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	batchset "k8s.io/client-go/kubernetes/typed/batch/v1"
+	batchlisters "k8s.io/client-go/listers/batch/v1"
+	corev1listers "k8s.io/client-go/listers/core/v1"
+	restclient "k8s.io/client-go/rest"
+
+	configapiv1 "github.com/openshift/api/config/v1"
+	imageregistryv1 "github.com/openshift/api/imageregistry/v1"
+	configlisters "github.com/openshift/client-go/config/listers/config/v1"
+	"github.com/openshift/cluster-image-registry-operator/pkg/defaults"
+	"github.com/openshift/cluster-image-registry-operator/pkg/storage/azure"
+)
+
+var _ Mutator = &generatorAzurePathFixJob{}
+
+type generatorAzurePathFixJob struct {
+	lister               batchlisters.JobNamespaceLister
+	secretLister         corev1listers.SecretNamespaceLister
+	infrastructureLister configlisters.InfrastructureLister
+	proxyLister          configlisters.ProxyLister
+	client               batchset.BatchV1Interface
+	cr                   *imageregistryv1.Config
+	kubeconfig           *restclient.Config
+}
+
+func NewGeneratorAzurePathFixJob(
+	lister batchlisters.JobNamespaceLister,
+	client batchset.BatchV1Interface,
+	secretLister corev1listers.SecretNamespaceLister,
+	infrastructureLister configlisters.InfrastructureLister,
+	proxyLister configlisters.ProxyLister,
+	cr *imageregistryv1.Config,
+	kubeconfig *restclient.Config,
+) *generatorAzurePathFixJob {
+	return &generatorAzurePathFixJob{
+		lister:               lister,
+		client:               client,
+		cr:                   cr,
+		infrastructureLister: infrastructureLister,
+		secretLister:         secretLister,
+		proxyLister:          proxyLister,
+		kubeconfig:           kubeconfig,
+	}
+}
+
+func (gapfj *generatorAzurePathFixJob) Type() runtime.Object {
+	return &batchv1.Job{}
+}
+
+func (gapfj *generatorAzurePathFixJob) GetNamespace() string {
+	return defaults.ImageRegistryOperatorNamespace
+}
+
+func (gapfj *generatorAzurePathFixJob) GetName() string {
+	return "azure-path-fix"
+}
+
+func (gapfj *generatorAzurePathFixJob) expected() (runtime.Object, error) {
+	azureCfg, err := azure.GetConfig(gapfj.secretLister, gapfj.infrastructureLister)
+	if err != nil {
+		return nil, err
+	}
+	clusterProxy, err := gapfj.proxyLister.Get(defaults.ClusterProxyResourceName)
+	if errors.IsNotFound(err) {
+		clusterProxy = &configapiv1.Proxy{}
+	} else if err != nil {
+		// TODO: should we report Degraded?
+		return nil, fmt.Errorf("unable to get cluster proxy configuration: %v", err)
+	}
+
+	azureStorage := gapfj.cr.Status.Storage.Azure
+	if azureStorage == nil {
+		return nil, fmt.Errorf("storage not yet provisioned")
+	}
+
+	envs := []corev1.EnvVar{
+		{Name: "AZURE_STORAGE_ACCOUNT_NAME", Value: azureStorage.AccountName},
+		{Name: "AZURE_CONTAINER_NAME", Value: azureStorage.Container},
+		{Name: "AZURE_CLIENT_ID", Value: azureCfg.ClientID},
+		{Name: "AZURE_TENANT_ID", Value: azureCfg.TenantID},
+		{Name: "AZURE_CLIENT_SECRET", Value: azureCfg.ClientSecret},
+		{Name: "AZURE_FEDERATED_TOKEN_FILE", Value: azureCfg.FederatedTokenFile},
+		{Name: "AZURE_ACCOUNTKEY", ValueFrom: &corev1.EnvVarSource{
+			SecretKeyRef: &corev1.SecretKeySelector{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: defaults.ImageRegistryPrivateConfiguration,
+				},
+				Key: "REGISTRY_STORAGE_AZURE_ACCOUNTKEY",
+			},
+		}},
+	}
+
+	if len(azureStorage.CloudName) > 0 {
+		envs = append(envs, corev1.EnvVar{Name: "AZURE_ENVIRONMENT", Value: azureStorage.CloudName})
+	}
+
+	if gapfj.cr.Spec.Proxy.HTTP != "" {
+		envs = append(envs, corev1.EnvVar{Name: "HTTP_PROXY", Value: gapfj.cr.Spec.Proxy.HTTP})
+	} else if clusterProxy.Status.HTTPProxy != "" {
+		envs = append(envs, corev1.EnvVar{Name: "HTTP_PROXY", Value: clusterProxy.Status.HTTPProxy})
+	}
+
+	if gapfj.cr.Spec.Proxy.HTTPS != "" {
+		envs = append(envs, corev1.EnvVar{Name: "HTTPS_PROXY", Value: gapfj.cr.Spec.Proxy.HTTPS})
+	} else if clusterProxy.Status.HTTPSProxy != "" {
+		envs = append(envs, corev1.EnvVar{Name: "HTTPS_PROXY", Value: clusterProxy.Status.HTTPSProxy})
+	}
+
+	if gapfj.cr.Spec.Proxy.NoProxy != "" {
+		envs = append(envs, corev1.EnvVar{Name: "NO_PROXY", Value: gapfj.cr.Spec.Proxy.NoProxy})
+	} else if clusterProxy.Status.NoProxy != "" {
+		envs = append(envs, corev1.EnvVar{Name: "NO_PROXY", Value: clusterProxy.Status.NoProxy})
+	}
+
+	// Cluster trusted certificate authorities - mount to /usr/share/pki/ca-trust-source/ to add
+	// CAs as low-priority trust sources. Registry runs update-ca-trust extract on startup, which
+	// merges the registry CAs with the cluster's trusted CAs into a single CA bundle.
+	//
+	// See man update-ca-trust for more information.
+	optional := true
+	trustedCAVolume := corev1.Volume{
+		Name: "trusted-ca",
+		VolumeSource: corev1.VolumeSource{
+			ConfigMap: &corev1.ConfigMapVolumeSource{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: defaults.TrustedCAName,
+				},
+				// Trust bundle is in PEM format - needs to be mounted to /anchors so that
+				// update-ca-trust extract knows that these CAs should always be trusted.
+				// This also ensures that no other low-priority trust is present in the container.
+				//
+				// See man update-ca-trust for more information.
+				Items: []corev1.KeyToPath{
+					{
+						Key:  "ca-bundle.crt",
+						Path: "anchors/ca-bundle.crt",
+					},
+				},
+				Optional: &optional,
+			},
+		},
+	}
+	trustedCAMount := corev1.VolumeMount{
+		Name:      trustedCAVolume.Name,
+		MountPath: "/usr/share/pki/ca-trust-source",
+	}
+	caTrustExtractedVolume := corev1.Volume{
+		Name: "ca-trust-extracted",
+		VolumeSource: corev1.VolumeSource{
+			EmptyDir: &corev1.EmptyDirVolumeSource{},
+		},
+	}
+	caTrustExtractedMount := corev1.VolumeMount{
+		Name:      "ca-trust-extracted",
+		MountPath: "/etc/pki/ca-trust/extracted",
+	}
+
+	backoffLimit := int32(0)
+	cj := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      gapfj.GetName(),
+			Namespace: gapfj.GetNamespace(),
+		},
+		Spec: batchv1.JobSpec{
+			BackoffLimit: &backoffLimit,
+			Template: kcorev1.PodTemplateSpec{
+				Spec: kcorev1.PodSpec{
+					RestartPolicy:      kcorev1.RestartPolicyNever,
+					ServiceAccountName: defaults.ServiceAccountName,
+					PriorityClassName:  "system-cluster-critical",
+					Containers: []kcorev1.Container{
+						{
+							Image: os.Getenv("OPERATOR_IMAGE"),
+							Resources: kcorev1.ResourceRequirements{
+								Requests: kcorev1.ResourceList{
+									kcorev1.ResourceCPU:    resource.MustParse("100m"),
+									kcorev1.ResourceMemory: resource.MustParse("256Mi"),
+								},
+							},
+							TerminationMessagePolicy: kcorev1.TerminationMessageFallbackToLogsOnError,
+							Env:                      envs,
+							VolumeMounts:             []corev1.VolumeMount{trustedCAMount, caTrustExtractedMount},
+							Name:                     gapfj.GetName(),
+							Command:                  []string{"/bin/sh"},
+							Args: []string{
+								"-c",
+								"mkdir -p /etc/pki/ca-trust/extracted/edk2 /etc/pki/ca-trust/extracted/java /etc/pki/ca-trust/extracted/openssl /etc/pki/ca-trust/extracted/pem && update-ca-trust extract && /usr/bin/move-blobs",
+							},
+						},
+					},
+					Volumes: []corev1.Volume{trustedCAVolume, caTrustExtractedVolume},
+				},
+			},
+		},
+	}
+
+	return cj, nil
+}
+
+func (gapfj *generatorAzurePathFixJob) Get() (runtime.Object, error) {
+	return gapfj.lister.Get(gapfj.GetName())
+}
+
+func (gapfj *generatorAzurePathFixJob) Create() (runtime.Object, error) {
+	return commonCreate(gapfj, func(obj runtime.Object) (runtime.Object, error) {
+		return gapfj.client.Jobs(gapfj.GetNamespace()).Create(
+			context.TODO(), obj.(*batchv1.Job), metav1.CreateOptions{},
+		)
+	})
+}
+
+func (gapfj *generatorAzurePathFixJob) Update(o runtime.Object) (runtime.Object, bool, error) {
+	return commonUpdate(gapfj, o, func(obj runtime.Object) (runtime.Object, error) {
+		return gapfj.client.Jobs(gapfj.GetNamespace()).Update(
+			context.TODO(), obj.(*batchv1.Job), metav1.UpdateOptions{},
+		)
+	})
+}
+
+func (gapfj *generatorAzurePathFixJob) Delete(opts metav1.DeleteOptions) error {
+	return gapfj.client.Jobs(gapfj.GetNamespace()).Delete(
+		context.TODO(), gapfj.GetName(), opts,
+	)
+}
+
+func (gapfj *generatorAzurePathFixJob) Owned() bool {
+	return true
+}
