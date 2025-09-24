340c2b6d0a61ffa72615347efcffec56b2de59d3
Deep Mistry <dmistry@redhat.com>
2025-09-24 16:16:12 -0400

    Fix the nil pointer dereference

--- pkg/operator/clusteroperator.go
+++ pkg/operator/clusteroperator.go
@@ -11,6 +11,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
+	"k8s.io/utils/clock"
 
 	configv1 "github.com/openshift/api/config/v1"
 	configv1client "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
@@ -131,6 +132,7 @@ func (c *ClusterOperatorStatusController) sync() error {
 		cr,
 		imagepruner,
 		c.relatedObjects,
+		clock.RealClock{},
 	)
 
 	return resource.ApplyMutator(mut)
