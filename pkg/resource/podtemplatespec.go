f1e4d9309db1e09ac0f4f800f69c1791485c95a5
Flavian Missi <fmissi@redhat.com>
2025-10-29 08:49:21 +0100

    wip: add a hash to deployment in attempt to generate a new version
    TestScaleUp is still failing, and I suspect it might be due to an
    improperly cleanup of the pod affinities.
    Try adding a hash annotation to force update when we want to clear up
    the pod affinities.

--- pkg/resource/podtemplatespec.go
+++ pkg/resource/podtemplatespec.go
@@ -3,6 +3,7 @@ package resource
 import (
 	"context"
 	"fmt"
+	"maps"
 	"os"
 	"strconv"
 	"strings"
@@ -443,10 +444,12 @@ func makePodTemplateSpec(coreClient coreset.CoreV1Interface, proxyLister configl
 	// then we fallback to a preferred affinity configuration. we only require a
 	// certain affinity during schedule if the number of replicas is defined to two.
 	var podAffinity *corev1.Affinity
+	var affinityHash string
 	desiredAffinity := cr.Spec.Affinity
 	if desiredAffinity != nil {
 		// user has explicitly set affinity in the config
 		podAffinity = desiredAffinity
+		affinityHash = "custom"
 	} else if cr.Spec.Replicas == 2 {
 		// for 2 replicas, set pod anti-affinity to ensure pods spread across nodes
 		podAffinity = &corev1.Affinity{
@@ -464,11 +467,14 @@ func makePodTemplateSpec(coreClient coreset.CoreV1Interface, proxyLister configl
 				},
 			},
 		}
+		affinityHash = "replicas-2"
 	} else {
-		// for other replica counts, explicitly set to empty struct to clear any
-		// existing affinity via strategic merge patch. setting to nil would cause
-		// the strategic merge patch to ignore the field and leave old affinity in place.
-		podAffinity = &corev1.Affinity{}
+		// for other replica counts, clear affinity by setting to nil.
+		// we also set an annotation to track this state change, since both nil and
+		// &corev1.Affinity{} produce the same JSON (field omitted due to omitempty),
+		// which means the spec hash won't detect the change.
+		podAffinity = nil
+		affinityHash = "none"
 	}
 
 	nodeSelectors := map[string]string{}
@@ -481,10 +487,14 @@ func makePodTemplateSpec(coreClient coreset.CoreV1Interface, proxyLister configl
 
 	gracePeriod := int64(55)
 
+	// add affinity hash annotation to ensure deployment updates when affinity changes
+	annotations := maps.Clone(defaults.DeploymentAnnotations)
+	annotations["imageregistry.operator.openshift.io/affinity-hash"] = affinityHash
+
 	spec := corev1.PodTemplateSpec{
 		ObjectMeta: metav1.ObjectMeta{
 			Labels:      defaults.DeploymentLabels,
-			Annotations: defaults.DeploymentAnnotations,
+			Annotations: annotations,
 		},
 		Spec: corev1.PodSpec{
 			Tolerations:       cr.Spec.Tolerations,
