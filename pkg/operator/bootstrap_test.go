69d19f0c679e0e837951404be283f30c01eb3766
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2026-01-30 13:51:10 +0100

    chore: implement unit test for the tls sync

--- pkg/operator/bootstrap_test.go
+++ pkg/operator/bootstrap_test.go
@@ -8,84 +8,48 @@ import (
 	"github.com/google/go-cmp/cmp"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/utils/clock"
 
 	configv1 "github.com/openshift/api/config/v1"
 	imageregistryv1 "github.com/openshift/api/imageregistry/v1"
 	operatorv1 "github.com/openshift/api/operator/v1"
-	configfakeclient "github.com/openshift/client-go/config/clientset/versioned/fake"
-	configinformers "github.com/openshift/client-go/config/informers/externalversions"
-	imageregistryfakeclient "github.com/openshift/client-go/imageregistry/clientset/versioned/fake"
-	imageregistryinformers "github.com/openshift/client-go/imageregistry/informers/externalversions"
-	"github.com/openshift/library-go/pkg/operator/events"
-
-	"github.com/openshift/cluster-image-registry-operator/pkg/client"
-	"github.com/openshift/cluster-image-registry-operator/pkg/configobservation"
 )
 
 func TestBootstrapAWS(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	configObjects := []runtime.Object{
-		&configv1.Infrastructure{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "cluster",
-			},
-			Status: configv1.InfrastructureStatus{
-				PlatformStatus: &configv1.PlatformStatus{
-					Type: configv1.AWSPlatformType,
-				},
+	setup := newTestControllerSetup(t)
+
+	// Add config objects
+	if err := setup.configClient.Tracker().Add(&configv1.Infrastructure{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster",
+		},
+		Status: configv1.InfrastructureStatus{
+			PlatformStatus: &configv1.PlatformStatus{
+				Type: configv1.AWSPlatformType,
 			},
 		},
-		&configv1.APIServer{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "cluster",
-			},
-		},
+	}); err != nil {
+		t.Fatalf("faile to add infrastructure to tracker: %v", err)
 	}
 
-	configClient := configfakeclient.NewSimpleClientset(configObjects...)
-	configInformerFactory := configinformers.NewSharedInformerFactory(configClient, 0)
-
-	imageregistryClient := imageregistryfakeclient.NewSimpleClientset()
-	imageregistryInformerFactory := imageregistryinformers.NewSharedInformerFactory(imageregistryClient, 0)
-
-	operatorClient := client.NewConfigOperatorClient(
-		imageregistryClient.ImageregistryV1().Configs(),
-		imageregistryInformerFactory.Imageregistry().V1().Configs(),
-	)
-
-	apiLister := configobservation.NewAPIServerConfigListers(
-		configInformerFactory.Config().V1().APIServers(),
-		operatorClient,
-	)
-
-	c := &Controller{
-		listers: &client.Listers{
-			StorageListers: client.StorageListers{
-				Infrastructures: configInformerFactory.Config().V1().Infrastructures().Lister(),
-			},
-			RegistryConfigs: imageregistryInformerFactory.Imageregistry().V1().Configs().Lister(),
+	if err := setup.configClient.Tracker().Add(&configv1.APIServer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster",
 		},
-		clients: &client.Clients{
-			RegOp: imageregistryClient,
-		},
-		apiLister:  apiLister,
-		evRecorder: events.NewInMemoryRecorder("bootstrap-test", clock.RealClock{}),
+	}); err != nil {
+		t.Fatalf("failed to add api server to tracker: %v", err)
 	}
 
-	configInformerFactory.Start(ctx.Done())
-	imageregistryInformerFactory.Start(ctx.Done())
-	configInformerFactory.WaitForCacheSync(ctx.Done())
-	imageregistryInformerFactory.WaitForCacheSync(ctx.Done())
+	// Start the controller
+	setup.start(t, ctx)
 
-	if err := c.Bootstrap(); err != nil {
+	if err := setup.controller.Bootstrap(); err != nil {
 		t.Fatalf("bootstrap failed: %v", err)
 	}
 
-	config, err := imageregistryClient.ImageregistryV1().Configs().Get(ctx, "cluster", metav1.GetOptions{})
+	config, err := setup.regClient.ImageregistryV1().Configs().Get(ctx, "cluster", metav1.GetOptions{})
 	if err != nil {
 		t.Fatal(err)
 	}
